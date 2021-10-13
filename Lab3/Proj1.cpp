2d74da0dc536784e30f8f4ce3d7e56569db530be
Faruhust <htv5815@gmail.com>
2021-10-13 15:47:33 +0300

    Update Proj1.cpp

--- Lab3/Proj1.cpp
+++ Lab3/Proj1.cpp
@@ -19,7 +19,7 @@ int main(void) {
 	ax = a * x;
 	t1 = (1 / (4 * pow(a, 3))) * log((pow(a, 2) + pow(x, 2)) / (pow(a, 2) - pow(x, 2)));
 	ax = a * x;
-	t2 = pow(cos(ax), n - 1) / (a * (m - 1) * pow(sin(ax), m - 1));
+	t2 = pow(cos(ax * 3.14 / 180), n - 1) / (a * (m - 1) * pow(sin(ax * 3.14 / 180 ), m - 1));
 
 	printf("t1 = %lg\n", t1);
 	printf("t2 = %lg\n", t2);
