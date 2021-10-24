6cdd113e9fd8dd4e7b2e5e61a613bc00e5465278
Faruhust <htv5815@gmail.com>
2021-10-24 19:29:27 +0300

    Lab 5
    Lab 5 , 5.1

--- Lab2/Proj1.cpp
+++ Lab2/Proj1.cpp
@@ -9,30 +9,28 @@ int main(void) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	char na1[9], na2[9], na3[9];
-	char type1, type2, type3;
-	unsigned long  celc1, celc2, celc3;
-	unsigned long sp1, sp2, sp3;
+	int na1[9], na2[9], na3[9];
+	char ruk1, ruk2, ruk3;
+	unsigned long  dia1, dia2, dia3;
+	unsigned long hz1, hz2, hz3;
 	/* Введение фактических данных*/
 	printf("1. Введите: Вещество, Тип, Температуру (град.С), Скорость (м/с) >");
-	scanf("%s %c %d %d", na1, &type1, &celc1, &sp1);
+	scanf("%s %s %d %d", na1, &ruk1, &dia1, &hz1);
 	printf("2. Введите: Вещество, Тип, Температуру (град.С), Скорость (м/с) >");
-	scanf("%s %c %d %d", na2, &type2, &celc2, &sp2);
+	scanf("%s %s %d %d", na2, &ruk2, &dia2, &hz2);
 	printf("3. Введите: Вещество, Тип, Температуру (град.С), Скорость (м/с) >");
-	scanf("%s %c %d %d", na3, &type3, &celc3, &sp3);
+	scanf("%s %s %d %d", na3, &ruk3, &dia3, &hz3);
 	/* Вывод таблицы */
 	/* вывод заголовков */
 	printf("|---------------------------------------------|\n");
 	printf("|         Скорость звука в жидкостях          |\n");
 	printf("|---------------------------------------------|\n");
-	printf("| Вещество |  Тип  |Температура|   Скорость   |\n");
-	printf("|          |       |  (град.С) |    (м/сек)   |\n");
+	printf("|   Год    |   Нацчный    |Диаметр|   Рабочая   |\n");
+	printf("|          | Руководитель |Антены |   Частота   |\n");
 	printf("|----------|-------|-----------|--------------|\n");
-	printf("| %-8s | %-5c | %-9d | %-12d |\n", na1, type1, celc1, sp1);
-	printf("| %-8s | %-5c | %-9d | %-12d |\n", na2, type2, celc2, sp2);
-	printf("| %-8s | %-5c | %-9d | %-12d |\n", na3, type3, celc3, sp3);
-	printf("|---------------------------------------------|\n");
-	printf("|      Ч - чистое вещество, М - масло;        |\n");
+	printf("| %-8s | %-5c | %-9d | %-12d |\n", na1, ruk1, dia1, hz1);
+	printf("| %-8s | %-5c | %-9d | %-12d |\n", na2, ruk2, dia2, hz2);
+	printf("| %-8s | %-5c | %-9d | %-12d |\n", na3, ruk3, dia3, hz3);
 	printf("|---------------------------------------------|\n");
 	return 0;
 }
